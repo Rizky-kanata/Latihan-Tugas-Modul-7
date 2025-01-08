452f6a137611e86b218c641ee0c9f191ffc985ab
Narendra Darel <narendra.darelrafi@gmail.com>
2025-01-08 17:02:59 +0700

    Inisiasi bagian Narendra Darel

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,6 +4,6 @@
     <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3
     border">
         <div class="bi-house-fill me-3 fs-1"></div>
-        <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
+        <h4 class="mb-0">Halo, ini modul 7 pengerjaan Narendra Darel, Terima kasih atas materi yang telah diberikan sehingga kami mahasiswa paham akan per git-git-an {{ $pageTitle }}.</h4>
     </div>
 </div>
